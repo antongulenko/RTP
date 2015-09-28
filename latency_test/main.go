bdbcc06b2173c71784504c720c6723c58e027ef2
Anton Gulenko <anton.gulenko@googlemail.com>
2015-09-28 13:58:05 +0200

    fix

--- latency_test/main.go
+++ latency_test/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"log"
+	"net"
 	"time"
 
 	. "github.com/antongulenko/RTP/helpers"
@@ -16,10 +17,12 @@ const (
 func main() {
 	target_addr := flag.String("target", default_target, "The partner to exchange latency measurement packets with")
 	local_addr := protocols.ParseCommandlineFlags("0.0.0.0", 6060)
+	local_host, _, err := net.SplitHostPort(local_addr)
+	Checkerr(err)
 
 	server, err := NewServer(local_addr)
 	Checkerr(err)
-	client, err := NewClient("0.0.0.0")
+	client, err := NewClient(local_host)
 	Checkerr(err)
 	err = client.SetServer(*target_addr)
 	Checkerr(err)
@@ -38,9 +41,9 @@ func main() {
 	}()
 
 	log.Println("Listening to Latency on " + local_addr + ", sending to: " + *target_addr)
-	log.Println("Press Ctrl-D to close")
+	log.Println("Press Ctrl-C to close")
 	WaitAndStopObservees(nil, []Observee{
 		server, stopSending,
-		&NoopObservee{StdinClosed()},
+		&NoopObservee{ExternalInterrupt()},
 	})
 }
