43313a0358d659f12982b9b04c870ba451910b07
Anton Gulenko <anton.gulenko@googlemail.com>
2015-10-09 19:52:51 +0200

    Added -num flag to rtpExperiment for executing multiple clients in parallel

--- rtpExperiment/proxies.go
+++ rtpExperiment/proxies.go
@@ -43,34 +43,32 @@ func closeProxyPCP(client *pcp.Client, listenPort, targetPort int) {
 
 func startProxies(rtp_port int) (string, int) {
 	proxy_ip := rtp_ip
-	if pretend_proxy {
-		return proxy_ip, proxy_port
-	}
-
-	if use_pcp {
-		proxy_ip = pcpProxyIp()
-		client, err := pcp.NewClientFor(pcp_url)
-		Checkerr(err)
-		log.Printf("Starting external proxies using %v\n", client)
-		makeProxyPCP(client, proxy_port, rtp_port)
-		makeProxyPCP(client, proxy_port+1, rtp_port+1)
-		observees = append(observees, CleanupObservee(func() {
-			closeProxyPCP(client, proxy_port, rtp_port)
-			closeProxyPCP(client, proxy_port+1, rtp_port+1)
-			Printerr(client.Close())
-		}))
-	} else {
-		proxy1 := makeProxy(proxy_port, rtp_port)
-		proxy2 := makeProxy(proxy_port+1, rtp_port+1)
-		statistics = append(statistics, proxy1.Stats, proxy2.Stats)
-		observees = append(observees, proxy1, proxy2, CleanupObservee(func() {
-			if proxy1.Err != nil {
-				log.Printf("Proxy %v error: %v\n", proxy_port, proxy1.Err)
-			}
-			if proxy2.Err != nil {
-				log.Printf("Proxy %v error: %v\n", proxy_port+1, proxy2.Err)
-			}
-		}))
+	if !pretend_proxy {
+		if use_pcp {
+			proxy_ip = pcpProxyIp()
+			client, err := pcp.NewClientFor(pcp_url)
+			Checkerr(err)
+			log.Printf("Starting external proxies using %v\n", client)
+			makeProxyPCP(client, proxy_port, rtp_port)
+			makeProxyPCP(client, proxy_port+1, rtp_port+1)
+			observees = append(observees, CleanupObservee(func() {
+				closeProxyPCP(client, proxy_port, rtp_port)
+				closeProxyPCP(client, proxy_port+1, rtp_port+1)
+				Printerr(client.Close())
+			}))
+		} else {
+			proxy1 := makeProxy(proxy_port, rtp_port)
+			proxy2 := makeProxy(proxy_port+1, rtp_port+1)
+			statistics = append(statistics, proxy1.Stats, proxy2.Stats)
+			observees = append(observees, proxy1, proxy2, CleanupObservee(func() {
+				if proxy1.Err != nil {
+					log.Printf("Proxy %v error: %v\n", proxy_port, proxy1.Err)
+				}
+				if proxy2.Err != nil {
+					log.Printf("Proxy %v error: %v\n", proxy_port+1, proxy2.Err)
+				}
+			}))
+		}
 	}
 	return proxy_ip, proxy_port
 }
